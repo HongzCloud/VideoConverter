b79d8fe6620dc205fe79f30aa8d826daac2fb3fd
HongzCloud <jihong21511@gmail.com>
2021-11-15 14:56:58 +0900

    💄[#14] CovertView 파일 생성

---
+++ VideoConverter/VideoConverter/View/ConvertView.swift
@@ -0,0 +1,20 @@
+//
+//  ConvertView.swift
+//  VideoConverter
+//
+//  Created by 오킹 on 2021/11/15.
+//
+
+import UIKit
+
+class ConvertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
