5b20e111e0964ea23e05196e92d2f068d4705be7
HongzCloud <jihong21511@gmail.com>
2021-11-22 16:44:19 +0900

    ✨[#18] 재생,멈춤,뒤로 10초,앞으로 10초 기능 구현

--- VideoConverter/VideoConverter/Controller/PlayerViewController.swift
+++ VideoConverter/VideoConverter/Controller/PlayerViewController.swift
@@ -11,8 +11,10 @@ import AVFoundation
 class PlayerViewController: UIViewController {
 
     private var playerView = PlayerView()
+    private var playerControlView = PlayerControlView()
     private var player: AVPlayer!
     private var playerLayer: AVPlayerLayer!
+    private var isPlaying = false
     private var tempButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "multiply"), for: .normal)
@@ -25,11 +27,12 @@ class PlayerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUIObject()
+        self.playerControlView.delegate = self
     }
         
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        player.play()
+ 
     }
     
     override func viewDidLayoutSubviews() {
@@ -40,6 +43,7 @@ class PlayerViewController: UIViewController {
     private func setUIObject() {
         setPlayerViewConstraints()
         setTempButtonConstraints()
+        setPlayerControlViewConstraints()
     }
     
     private func setPlayerViewConstraints() {
@@ -54,6 +58,20 @@ class PlayerViewController: UIViewController {
             self.playerView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
             self.playerView.heightAnchor.constraint(equalTo: self.playerView.widthAnchor, multiplier: 9/16)
         ])
+    }
+    
+    private func setPlayerControlViewConstraints() {
+        playerControlView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(playerControlView)
+        
+        let safeArea = self.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            self.playerControlView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -20),
+            self.playerControlView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            self.playerControlView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            self.playerControlView.heightAnchor.constraint(equalTo: safeArea.heightAnchor, multiplier: 0.15)
+        ])
+       
     }
     
     private func setTempButtonConstraints() {
@@ -78,16 +96,45 @@ class PlayerViewController: UIViewController {
         
         self.playerView.layer.addSublayer(playerLayer)
     }
-    
-    
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PlayerViewController: PlayerControlViewDelegate {
+    func didTappedPlayButton(_ button: UIButton) {
+        if isPlaying {
+            button.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+            player.pause()
+        } else {
+            button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+            player.play()
+        }
+        
+        isPlaying = !isPlaying
     }
-    */
 
+    func didTappedBackwardButton() {
+        let currentTime = CMTimeGetSeconds(player.currentTime())
+        var newTime = currentTime - 10.0
+        
+        if newTime < 0 {
+            newTime = 0
+        }
+        
+        let time = CMTimeMake(value: Int64(newTime * 1000), timescale: 1000)
+        player.seek(to: time)
+    }
+    
+    func didTappedForwardButton() {
+        let currentTime = CMTimeGetSeconds(player.currentTime())
+        var newTime = currentTime + 10.0
+        let duration = self.player.currentItem!.asset.duration
+        
+        if newTime > CMTimeGetSeconds(duration) {
+            newTime = CMTimeGetSeconds(duration)
+        }
+        
+        let time = CMTimeMake(value: Int64(newTime * 1000), timescale: 1000)
+        player.seek(to: time)
+    }
+    
+    
 }
---
+++ VideoConverter/VideoConverter/View/PlayerControlView.swift
@@ -0,0 +1,131 @@
+//
+//  PlayerControlView.swift
+//  VideoConverter
+//
+//  Created by 오킹 on 2021/11/22.
+//
+
+import UIKit
+
+protocol PlayerControlViewDelegate: AnyObject {
+    func didTappedPlayButton(_ button: UIButton)
+    func didTappedBackwardButton()
+    func didTappedForwardButton()
+}
+
+class PlayerControlView: UIView {
+
+    weak var delegate: PlayerControlViewDelegate?
+    
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        let symbolSize = UIImage.SymbolConfiguration.init(pointSize: 30)
+        button.setPreferredSymbolConfiguration(symbolSize, forImageIn: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(didTappedPlayButton(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let backwardButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "gobackward.10"), for: .normal)
+        let symbolSize = UIImage.SymbolConfiguration.init(pointSize: 30)
+        button.setPreferredSymbolConfiguration(symbolSize, forImageIn: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(didTappedBackwardButton(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let forwardButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "goforward.10"), for: .normal)
+        let symbolSize = UIImage.SymbolConfiguration.init(pointSize: 30)
+        button.setPreferredSymbolConfiguration(symbolSize, forImageIn: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(didTappedForwardButton(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let durationSlider: UISlider = {
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        return slider
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUIObject()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUIObject()
+    }
+    
+    private func setUIObject() {
+        setPlayButtonConstraints()
+        setDurationSlider()
+        setBackwardButton()
+        setForwardButton()
+    }
+    
+    private func setPlayButtonConstraints() {
+        self.addSubview(playButton)
+        
+        NSLayoutConstraint.activate([
+            self.playButton.topAnchor.constraint(equalTo: self.centerYAnchor),
+            self.playButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.playButton.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+    }
+    
+    private func setDurationSlider() {
+        self.addSubview(durationSlider)
+
+        NSLayoutConstraint.activate([
+            self.durationSlider.topAnchor.constraint(equalTo: self.topAnchor),
+            self.durationSlider.bottomAnchor.constraint(equalTo: self.playButton.topAnchor),
+            self.durationSlider.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+  
+            self.durationSlider.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8)
+        ])
+    }
+    
+    private func setBackwardButton() {
+        self.addSubview(backwardButton)
+        
+        NSLayoutConstraint.activate([
+            self.backwardButton.centerYAnchor.constraint(equalTo: self.playButton.centerYAnchor),
+            self.backwardButton.heightAnchor.constraint(equalTo: self.playButton.heightAnchor),
+            self.backwardButton.widthAnchor.constraint(equalTo: self.playButton.widthAnchor),
+            self.backwardButton.trailingAnchor.constraint(equalTo: self.playButton.leadingAnchor, constant: -20)
+        ])
+    }
+    
+    private func setForwardButton() {
+        self.addSubview(forwardButton)
+        
+        NSLayoutConstraint.activate([
+            self.forwardButton.centerYAnchor.constraint(equalTo: self.playButton.centerYAnchor),
+            self.forwardButton.heightAnchor.constraint(equalTo: self.playButton.heightAnchor),
+            self.forwardButton.widthAnchor.constraint(equalTo: self.playButton.widthAnchor),
+            self.forwardButton.leadingAnchor.constraint(equalTo: self.playButton.trailingAnchor, constant: 20)
+        ])
+    }
+    
+    @objc func didTappedPlayButton(_ sender: UIButton!) {
+        self.delegate?.didTappedPlayButton(playButton)
+    }
+    
+    @objc func didTappedBackwardButton(_ sender: UIButton!) {
+        self.delegate?.didTappedBackwardButton()
+    }
+    
+    @objc func didTappedForwardButton(_ sender: UIButton!) {
+        self.delegate?.didTappedForwardButton()
+    }
+}
