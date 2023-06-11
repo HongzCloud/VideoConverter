f938bc658055d7f1b80491eeedbf48b034fe69fc
HongzCloud <jihong21511@gmail.com>
2023-06-11 23:56:22 +0900

    :white_check_mark: XcodeCloud Test 실패를 확인하기 위한 파일 추가

---
+++ VideoConverter/VideoConverterTests/UnitTestForXcodeCloudTestFail.swift
@@ -0,0 +1,15 @@
+//
+//  UnitTestForXcodeCloudTestFail.swift
+//  VideoConverterTests
+//
+//  Created by 오킹 on 2023/06/11.
+//
+
+import XCTest
+
+final class UnitTestForXcodeCloudTestFail: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertTrue(false)
+    }
+}
